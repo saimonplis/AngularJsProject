7781386e3e808f13b6786099a47e417acbd54bbe
Simone <simo93sr@hotmail.it>
2016-10-29 13:37:18 +0200

    filters, empty messages, dialog enhanced. to fix: template dialog

--- app/components/todoApp.module.js
+++ app/components/todoApp.module.js
@@ -1,6 +1,6 @@
 (function(angular){
     'use strict'
-    var module = angular.module('todoApp', ['ngMaterial']);
+    var module = angular.module('todoApp', ['ngMaterial','ngAnimate']);
 
 
 })(window.angular);
