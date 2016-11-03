43e7eee23ed8ad2740f977373632b6b087b2e21b
neme92 <vincenzorin@gmail.com>
2016-11-03 17:52:13 +0100

    Dialog di inserimento: cliccare fuori ora chiude la dialog

--- app/components/todo.controller.js
+++ app/components/todo.controller.js
@@ -1,4 +1,4 @@
-(function(angular) {
+(function (angular) {
 
     'use strict';
 
@@ -7,40 +7,40 @@
     function TodoController(storageService, $mdDialog) {
         var vm = this;
         var priorities = {
-            lowpriority:0,
-            highpriority:1
+            lowpriority: 0,
+            highpriority: 1
         };
-        vm.tags=[];
-        vm.priorities=priorities;
-       
+        vm.tags = [];
+        vm.priorities = priorities;
+
         vm.selectedItem = null;
         vm.items = storageService.get() || [];
 
-        vm.notDone = function(item) {
+        vm.notDone = function (item) {
             return item.done == false;
         }
 
-        vm.done = function(item) {
+        vm.done = function (item) {
             return item.done == true;
         }
 
-        vm.all = function(item) {
+        vm.all = function (item) {
             return true;
         }
-       
+
 
         //Delete the current selected item, if any
-        vm.deleteItem = function(ev) {
+        vm.deleteItem = function (ev) {
 
             if (vm.selectedItem != null) {
                 var confirm = $mdDialog.confirm()
-                                       .textContent('The task "' + vm.selectedItem.title + '" will be deleted. Are you sure?')
-                                       .ariaLabel('Delete task')
-                                       .targetEvent(ev)
-                                       .ok('Yes')
-                                       .cancel('No');
+                    .textContent('The task "' + vm.selectedItem.title + '" will be deleted. Are you sure?')
+                    .ariaLabel('Delete task')
+                    .targetEvent(ev)
+                    .ok('Yes')
+                    .cancel('No');
 
-                $mdDialog.show(confirm).then(function(result) {
+                $mdDialog.show(confirm).then(function (result) {
                     if (result) {
                         var index = vm.items.indexOf(vm.selectedItem);
                         if (index != -1) {
@@ -55,35 +55,35 @@
         }
 
         vm.deleteSingleTask = function (ev) {
-                var confirm = $mdDialog.confirm()
+            var confirm = $mdDialog.confirm()
 
-                    .textContent('Are you sure to delete?')
-                    .ariaLabel('Delete task')
-                    .targetEvent(ev)
-                    .ok('Yes')
-                    .cancel('No');
+                .textContent('Are you sure to delete?')
+                .ariaLabel('Delete task')
+                .targetEvent(ev)
+                .ok('Yes')
+                .cancel('No');
 
-                $mdDialog.show(confirm).then(function (result) {
-                    if (result) {
-                        if (vm.indice != -1) {
-                            vm.items.splice(vm.indice, 1);
-                            storageService.set(vm.items);
-                        }
+            $mdDialog.show(confirm).then(function (result) {
+                if (result) {
+                    if (vm.indice != -1) {
+                        vm.items.splice(vm.indice, 1);
+                        storageService.set(vm.items);
                     }
-                    vm.selectedItem = null;
-                });
+                }
+                vm.selectedItem = null;
+            });
         }
 
         //Creates a new item with the given parameters
-        vm.createItem = function(task) {
-            
+        vm.createItem = function (task) {
+
             vm.items.push({
                 title: task.title,
                 description: task.description,
                 done: task.done || false,
                 priority: task.priority || 0,
                 tag: task.tags || 'generic',
-                ework: task.ework ||'notspecified',
+                ework: task.ework || 'notspecified',
                 date: task.date || Date.now()
             });
             storageService.set(vm.items);
@@ -92,63 +92,64 @@
 
 
         //Add a new task to the items list 
-        vm.addTask = function(ev) {
-                 showDialog(ev);
+        vm.addTask = function (ev) {
+            showDialog(ev);
         };
-      
+
         function showDialog($event) {
             var parentEl = angular.element(document.body);
             $mdDialog.show({
-              parent: parentEl,
-              targetEvent: $event,
-              templateUrl: 'app/components/customDialog.template.html',
-              locals: {
-                items: vm.items,
-                priorities: vm.priorities,
-                tags:vm.tags,
-              },
-              controller: DialogController,
-              controllerAs: 'dctrl',
+                parent: parentEl,
+                targetEvent: $event,
+                templateUrl: 'app/components/customDialog.template.html',
+                locals: {
+                    items: vm.items,
+                    priorities: vm.priorities,
+                    tags: vm.tags
+                },
+                controller: DialogController,
+                controllerAs: 'dctrl',
+                clickOutsideToClose: true,
 
-            }).then(function(task){
-                
+            }).then(function (task) {
+
                 vm.createItem(task);
-                vm.tags=[];
+                vm.tags = [];
             });
         };
-    
 
-        function DialogController($mdDialog, items, priorities,tags) {
-            var vm=this;
+
+        function DialogController($mdDialog, items, priorities, tags) {
+            var vm = this;
             vm.items = items;
-            vm.priorities=priorities;
-            
-            vm.tags=tags;
+            vm.priorities = priorities;
+
+            vm.tags = tags;
             console.log(vm.tags);
-            vm.editableTags=angular.copy(vm.tags);
-            vm.removable=true;
-            vm.readonly=false;
-            vm.closeDialog = function() {
-              $mdDialog.cancel();
+            vm.editableTags = angular.copy(vm.tags);
+            vm.removable = true;
+            vm.readonly = false;
+            vm.closeDialog = function () {
+                $mdDialog.cancel();
             }
-            vm.createTask=function(title,description,priority,date,tags){  
-                
-                var task={
-                    title:title,
-                    description:description,
+            vm.createTask = function (title, description, priority, date, tags) {
+
+                var task = {
+                    title: title,
+                    description: description,
                     priority: priority,
                     date: date,
-                    tags:tags,
+                    tags: tags,
 
                 };
-                
-                
+
+
                 $mdDialog.hide(task);
             }
-            vm.newTag=function(chip){
+            vm.newTag = function (chip) {
                 vm.tags.push(chip);
                 console.log(vm.tags);
             }
-        };  
-    }      
+        };
+    }
 })(window.angular);
